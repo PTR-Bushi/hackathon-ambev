5bb7a1f8883c2f601d8c772add60c310a0c413f4
Pedro <ptr.bushi@hotmail.com>
2020-12-13 22:44:01 -0300

    :card_file_box: feat: add new fields to weather model

--- weather/models.py
+++ weather/models.py
@@ -5,8 +5,13 @@ class Weather(models.Model):
 	city_id = models.IntegerField(default=0)
 	city_name = models.CharField(max_length=50, default="")
 	temperature = models.FloatField(default=0)
+	t_max = models.FloatField(default=0)
+	t_min = models.FloatField(default=0)
 	humidity = models.FloatField(default=0)
 	date_taken = models.DateTimeField(auto_now_add=True, null=True)
+	utc_time = models.IntegerField(default=0)
+	precipitation = models.FloatField(default=0)
+	solar_rad = models.FloatField(default=0)
 
 	def __str__(self):
 		return "id:{}, at:{}, temp:{}, time:{}".format(self.id, self.city_name, self.temperature, self.date_taken)
